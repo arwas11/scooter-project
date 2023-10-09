58fe12492feb092c946a1f498408e9b5a25d74d3
Arwa Sharif <arwas@google.com>
2023-10-09 11:39:30 -0500

    Sooter and User methods tested and passed

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,60 +1,55 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
+describe("scooter object", () => {
+  test("Scooter class should create Scooter instance", () => {
     const scooter = new Scooter();
     expect(scooter).toBeInstanceOf(Scooter);
   });
-
-  
-})
+});
 
 //Method tests
-describe('scooter methods', () => {
+describe("scooter methods", () => {
   // tests here!
 
   //rent method
-  test('Successful renting of a scooter to a user', () => {
-    const user1 = new User('test', 'test123', 20);
-    const scooter1 = new Scooter('CHI');
+  test("Successful renting of a scooter to a user", () => {
+    const user1 = new User("test", "test123", 20);
+    const scooter1 = new Scooter("CHI");
     scooter1.rent(user1);
     expect(scooter1.user).toBe(user1);
-    expect(scooter1.station).toBe(null)
-  })
-
-  test('rent fails due to low battery', () => {
-    const scooter = new Scooter('NYC');
-    scooter.charge = 15;
-    const user = new User('test', 'test123', 20);
-    expect(() => scooter.rent(user))
-      .toThrow('scooter needs to charge');
-    expect(scooter.user).toBeNull();
-    expect(scooter.station).toBe('NYC');
+    expect(scooter1.station).toBe(null);
   });
 
-  test('rent fails due to broken scooter', () => {
-    const scooter = new Scooter('NYC');
-    scooter.isBroken = true;
-    const user = new User('test', 'test123', 10);
-    expect(() => scooter.rent(user))
-      .toThrow('scooter needs repair');
-    expect(scooter.user).toBeNull();
-    expect(scooter.station).toBe('NYC');
+  test("rent fails due to low battery", () => {
+    const scooter1 = new Scooter("NYC");
+    scooter1.charge = 15;
+    const user1 = new User("test", "test123", 20);
+    expect(() => scooter1.rent(user1)).toThrow("scooter needs to charge");
+    expect(scooter1.user).toBeNull();
+    expect(scooter1.station).toBe("NYC");
+  });
+
+  test("rent fails due to broken scooter", () => {
+    const scooter1 = new Scooter("NYC");
+    scooter1.isBroken = true;
+    const user1 = new User("test", "test123", 10);
+    expect(() => scooter1.rent(user1)).toThrow("scooter needs repair");
+    expect(scooter1.user).toBeNull();
+    expect(scooter1.station).toBe("NYC");
   });
 
   //dock method
-  test('dock succeeds', () => {
-    const scooter = new Scooter('Milford Mill');
-    const user = new User('test', 'test123', 40);
-    scooter.rent(user);
-    scooter.dock('West Loop');
-    expect(scooter.user).toBeNull();
-    expect(scooter.station).toBe('West Loop');
+  test("dock succeeds", () => {
+    const scooter1 = new Scooter("Milford Mill");
+    const user1 = new User("test", "test123", 40);
+    scooter1.rent(user1);
+    scooter1.dock("West Loop");
+    expect(scooter1.user).toBeNull();
+    expect(scooter1.station).toBe("West Loop");
   });
   //requestRepair method
 
   //charge method
-
-})
+});
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,19 +1,35 @@
-const User = require('../src/User');
+const User = require("../src/User");
 
-const user = new User("Joe Bloggs", "test123", 21);
+const user1 = new User("Joe Bloggs", "test123", 21);
+const user2 = new User("testing", "testing123", 50);
 
 // User tests here
 describe("User property tests", () => {
   // test username
   test("username should be a string", () => {
-    expect(typeof user.username).toBe("string");
-  })
+    expect(typeof user1.username).toBe("string");
+  });
   // test password
 
   // test age
-})
+});
 
+describe("User methods tests", () => {
+  // test successful login
+  test("user logged in", () => {
+    expect(user1.login("test123")).toBe();
+  });
 
-// test login
+  // test unsuccessful login
 
-// test logout
+  test("user fails log in due to incorrect password", () => {
+    expect(() => user2.login("test123")).toThrow("incorrect password");
+  });
+  // test logout
+
+  test(" user logged out", () => {
+    user1.login("test123");
+    user1.logout();
+    expect(user1.loggedIn).toBe(false);
+  });
+});
